24b82820fd9b6f481d1a7e47896756cadfe21601
Aref <aref.dehghanimo@gmail.com>
2025-05-08 00:57:11 -0700

    first API request

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const axios = require("axios");
+
+axios.get('https://lm-models.s3.ir-thr-at1.arvanstorage.ir/cars.json')
+.then((res) =>{
+    console.log(res);
+})
+.catch((error) =>{
+    console.log(error)
+})
